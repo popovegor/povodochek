55cd3c847852824df5d62e07dfc4a67861b7cefa
egor <egor@egor-pc-podovochek-deb7.ri.domain>
2014-04-18 18:16:23 +0400

    добавлен значок чемпионские крови

--- src/static/js/povodochek.js
+++ src/static/js/povodochek.js
@@ -121,20 +121,20 @@ povodochek.typeahead = function(ajax_url, input_id, updater){
 };
 
 
-(function($) {
+// (function($) {
 
-    var oldHide = $.fn.popover.Constructor.prototype.hide;
+//     var oldHide = $.fn.popover.Constructor.prototype.hide;
 
-    $.fn.popover.Constructor.prototype.hide = function() {
-        if (this.options.trigger === "hover" && this.tip().is(":hover")) {
-            var that = this;
-            // try again after what would have been the delay
-            setTimeout(function() {
-                return that.hide.call(that, arguments);
-            }, that.options.delay.hide);
-            return;
-        }
-        oldHide.call(this, arguments);
-    };
+//     $.fn.popover.Constructor.prototype.hide = function() {
+//         if (this.options.trigger === "hover" && this.tip().is(":hover")) {
+//             var that = this;
+//             // try again after what would have been the delay
+//             setTimeout(function() {
+//                 return that.hide.call(that, arguments);
+//             }, that.options.delay.hide);
+//             return;
+//         }
+//         oldHide.call(this, arguments);
+//     };
 
-})(jQuery);
+// })(jQuery);
