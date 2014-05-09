113bd1c99a5e8595c2c182daf8a650f69be43480
egor <egor@egor-pc-podovochek-deb7.ri.domain>
2014-05-10 00:43:46 +0400

    исправлен обработчик 500 ошибки

--- src/app.py
+++ src/app.py
@@ -1294,7 +1294,7 @@ def page_not_found(e):
     return render_template('404.html', title = u"Запрашиваемая вами страница не найдена"), 404
 
 @app.errorhandler(500)
-def page_not_found(e):
+def internal_server_error(e):
     return render_template('500.html', title = u"Упс, у нас на сервере произошла ошибка!"), 500
 
 
